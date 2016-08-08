6d186370bf9007292d420d7006492c672db4e77a
Rosalie Tolentino <rosatolen@thoughtworks.com>
2016-08-08 15:00:09 -0500

    R & N | 1 | Run real resolver test on travis

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#include "resolver.h"
+
 int main() {
     printf("Hello World! I'm a toy.");
+    printf(resolve("www.thoughtworks.com"));
 }
---
+++ src/resolver.h
@@ -0,0 +1,1 @@
+char* resolve(char*);
