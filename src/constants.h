7fad3a5a7849fda806daf814c5d761d35b2b10bc
Chelsea H. Komlo <chelsea.komlo@gmail.com>
2016-08-15 15:27:33 -0500

    C | 7 | correct defines

--- src/constants.h
+++ src/constants.h
@@ -1,16 +1,16 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define uint16_t QUERY = 0;
-#define uint16_t STANDARD_QUERY = 0;
-#define uint16_t NOT_AUTHORITATIVE = 0;
-#define uint16_t NOT_TRUNCATED = 0;
-#define uint16_t RECURSION_DESIRED = 1;
-#define uint16_t RECURSION_NOT_AVAILABLE = 0;
-#define uint16_t NO_ERRORS = 0;
-#define uint16_t ONE_QUESTION = 1;
-#define uint16_t NO_RESOURCE_RECORDS_IN_ANSWER = 0;
-#define uint16_t NO_RESOURCE_RECORDS_IN_AUTHORITY = 0;
-#define uint16_t NO_RESOURCE_RECORDS_IN_ADDITIONAL = 0;
+#define QUERY 0;
+#define STANDARD_QUERY 0;
+#define NOT_AUTHORITATIVE 0;
+#define NOT_TRUNCATED 0;
+#define RECURSION_DESIRED 1;
+#define RECURSION_NOT_AVAILABLE 0;
+#define NO_ERRORS 0;
+#define ONE_QUESTION 1;
+#define NO_RESOURCE_RECORDS_IN_ANSWER 0;
+#define NO_RESOURCE_RECORDS_IN_AUTHORITY 0;
+#define NO_RESOURCE_RECORDS_IN_ADDITIONAL 0;
 
 #endif
